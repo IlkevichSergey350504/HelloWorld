166a82adb4054d3ff6fb2031b843bfe46984f679
Kevich <kevich.mz@gmail.com>
2015-12-14 00:36:47 +0300

    4

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,9 +13,9 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
-
-        
         primaryStage.show();
+
+        //rvc
     }
 
 
